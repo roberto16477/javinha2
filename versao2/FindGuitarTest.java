c9febb46561150cea3111394fc53d8abe82ba94e
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2025-03-25 20:12:44 +0000

    versão 3

--- versao2/FindGuitarTest.java
+++ versao2/FindGuitarTest.java
@@ -5,21 +5,21 @@ public class FindGuitarTest {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
         initializeInventory(inventory);
-    
+
         GuitarSpec whatErinLikes = 
-            new GuitarSpec(Builder.FENDER, "Stratocastor", Type.ELECTRIC, 
+            new GuitarSpec(Builder.FENDER, "Stratocastor", Type.ELECTRIC, 6, 
                            Wood.ALDER, Wood.ALDER);
-    
-        List matchingGuitars = inventory.search(whatErinLikes);
+
+        List<Guitar> matchingGuitars = inventory.search(whatErinLikes);
         if (!matchingGuitars.isEmpty()) {
             System.out.println("Erin, you might like these guitars:");
-            for (Iterator i = matchingGuitars.iterator(); i.hasNext(); ) {
-                Guitar guitar = (Guitar) i.next();
+            for (Guitar guitar : matchingGuitars) {
                 GuitarSpec spec = guitar.getSpec();
-    
+
                 System.out.println("We have a " + 
                     spec.getBuilder() + " " + spec.getModel() + " " +
                     spec.getType() + " guitar:\n" +
+                    spec.getNumStrings() + " strings,\n" +
                     spec.getBackWood() + " back and sides,\n" +
                     spec.getTopWood() + " top. You can have it for only $" + 
                     guitar.getPrice() + "!\n ----");
@@ -28,10 +28,10 @@ public class FindGuitarTest {
             System.out.println("Sorry, Erin, we have nothing for you.");
         }
     }
- 
+
     private static void initializeInventory(Inventory inventory) {
-        inventory.addGuitar("E0705", 1499.99, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
-        inventory.addGuitar("E1310", 1549.99, Builder.GIBSON, "Les Paul", Type.ELECTRIC, Wood.MAHOGANY, Wood.MAPLE);
-        inventory.addGuitar("V1503", 1299.99, Builder.MARTIN, "D-28", Type.ACOUSTIC, Wood.INDIAN_ROSEWOOD, Wood.CEDAR);
+        inventory.addGuitar("E0705", 1499.99, Builder.FENDER, "Stratocastor", Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
+        inventory.addGuitar("E1310", 1549.99, Builder.GIBSON, "Les Paul", Type.ELECTRIC, 6, Wood.MAHOGANY, Wood.MAPLE);
+        inventory.addGuitar("V1503", 1299.99, Builder.MARTIN, "D-28", Type.ACOUSTIC, 6, Wood.INDIAN_ROSEWOOD, Wood.CEDAR);
     }
 }
--- versao2/GuitarSpec.java
+++ versao2/GuitarSpec.java
@@ -3,14 +3,33 @@ public class GuitarSpec {
     private String model;
     private Type type;
     private Wood backWood, topWood;
+    private int numStrings;
 
-    public GuitarSpec(Builder builder, String model, Type type,
+    public GuitarSpec(Builder builder, String model, Type type, int numStrings,
     Wood backWood, Wood topWood){
     this.builder = builder;
     this.model = model;
     this.type = type;
+    this.numStrings = numStrings;
     this.backWood = backWood;
     this.topWood = topWood;
+    }
+
+    public boolean matches(GuitarSpec otherSpec) {
+        if (builder != otherSpec.builder)
+            return false;
+        if ((model != null) && (!model.equals("")) && 
+            (!model.equals(otherSpec.model)))
+            return false;
+        if (type != otherSpec.type)
+            return false;
+        if (numStrings != otherSpec.numStrings)
+            return false;
+        if (backWood != otherSpec.backWood)
+            return false;
+        if (topWood != otherSpec.topWood)
+            return false;
+        return true;
     }
 
     public Builder getBuilder(){
@@ -20,8 +39,13 @@ public class GuitarSpec {
     public String getModel(){
     return model;
     }
+
     public Type getType(){
     return type;
+    }
+
+    public int getNumStrings(){
+        return numStrings;
     }
 
     public Wood getBackWood(){
