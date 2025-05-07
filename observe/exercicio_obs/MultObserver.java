667f2ea4c32700326f5de6f0cbfa633bd647312e
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2025-05-07 13:58:24 +0000

    up final

--- observe/exercicio_obs/MultObserver.java
+++ observe/exercicio_obs/MultObserver.java
@@ -1,6 +1,6 @@
 package exercicio_obs;
 
-public class MultObserver {
+public class MultObserver implements Observer {
 
     public void update(int valor1, int valor2) {
         System.out.println("Multiplicação: " + (valor1 * valor2));
